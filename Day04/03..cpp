72d73461e542c767e2b7175576c174ff4547ef12
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2025-02-10 15:30:41 +0530

    day 05 commit's

---
+++ Day04/03..cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //  find the character lowercase or uppercase
+
+    char ch = 'A';
+
+    if(ch >= 'a' & ch <= 'z'){
+        cout<<"lowercase "<<endl;
+    }
+    else{
+        cout<<"uppercase "<<endl;
+    }
+
+    return 0;
+}
